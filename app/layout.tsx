c29136cca20c313ede8d73be0aae290f13e714b1
N-feng <308561157@qq.com>
2024-11-27 19:51:17 +0800

    init🎉: first commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - AI Resume Builder",
+    absolute: "AI Resume Builder",
+  },
+  description:
+    "AI Resume Builder is the easiest way to create a professional resume that will help you land your dream job.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className} suppressHydrationWarning>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
